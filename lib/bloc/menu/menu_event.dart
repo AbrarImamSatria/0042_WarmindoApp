a9ebcd6098c3f8a1f7193ab68b7f047b2714f4d1
abrarimamsatria09 <092004abrar@gmail.com>
2025-06-26 23:18:35 +0700

    membuat menu event

---
+++ lib/bloc/menu/menu_event.dart
@@ -0,0 +1,53 @@
+part of 'menu_bloc.dart';
+
+@immutable
+sealed class MenuEvent {}
+
+// Load all menu
+final class MenuLoad extends MenuEvent {}
+
+// Load menu by category
+final class MenuLoadByCategory extends MenuEvent {
+  final String kategori;
+
+  MenuLoadByCategory({required this.kategori});
+}
+
+// Search menu
+final class MenuSearch extends MenuEvent {
+  final String keyword;
+
+  MenuSearch({required this.keyword});
+}
+
+// Add menu
+final class MenuAdd extends MenuEvent {
+  final MenuModel menu;
+
+  MenuAdd({required this.menu});
+}
+
+// Update menu
+final class MenuUpdate extends MenuEvent {
+  final MenuModel menu;
+
+  MenuUpdate({required this.menu});
+}
+
+// Update foto menu
+final class MenuUpdateFoto extends MenuEvent {
+  final int menuId;
+  final String? fotoPath;
+
+  MenuUpdateFoto({
+    required this.menuId,
+    required this.fotoPath,
+  });
+}
+
+// Delete menu
+final class MenuDelete extends MenuEvent {
+  final int menuId;
+
+  MenuDelete({required this.menuId});
+}
