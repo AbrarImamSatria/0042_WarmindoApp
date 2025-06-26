81771a5002c4fc6f5d2f375553d50cec88c3f5f4
abrarimamsatria09 <092004abrar@gmail.com>
2025-06-26 13:52:59 +0700

    menambahkan library yang diperlukan

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "abrarimam.id.warmindo_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "29.0.13113456"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
